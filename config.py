0b8b9f0ac2cb30ac78e29e635c22e0fb71641492
Rohit Kumar <67968330+iCriptSnappie@users.noreply.github.com>
2024-06-01 18:21:25 +0530

    Update config.py

--- config.py
+++ config.py
@@ -31,7 +31,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/itz-rishu/VIP-MUSIC",
+    "https://github.com/iCriptSnappie/Vip_Music",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
